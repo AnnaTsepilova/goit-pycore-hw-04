c94236abb446d523d7e6c9621d5b7b77363a18ae
Anna Tsepilova <virgo.tsav@gmail.com>
2024-07-09 19:50:05 +0300

    Created scanning logic

--- task_3/main.py
+++ task_3/main.py
@@ -1,9 +1,32 @@
-import sys
+import sys, os
 from pathlib import Path
 from colorama import Fore, Back, Style
 
-def parseFolder(path: str):
-    print(path)
+def parseFolder(path: str, deep: int = 1):
+    directory = Path(path)
+    if not directory.is_dir():
+        print(f"{Fore.RED}{Style.BRIGHT}Error: incorrect path{Style.RESET_ALL}")
+        return False
+
+    tab = '  ' * deep
+
+    if deep == 1:
+        print(f"{Fore.BLUE}{Style.BRIGHT}{path}/{Style.RESET_ALL}")
+
+    for node in directory.iterdir():
+        p_node = Path(node)
+        relative_path = p_node.relative_to(path)
+
+        if p_node.is_dir():
+            print(f"{tab}{Fore.BLUE}{Style.BRIGHT}{relative_path}/{Style.RESET_ALL}")
+            parseFolder(node, deep + 1)
+        elif p_node.is_symlink():
+            target = os.readlink(node)
+            print(f"{tab}{Fore.CYAN}{node} -> {target}{Fore.RESET}")
+        elif p_node.is_file():
+            print(f"{tab}{Fore.LIGHTGREEN_EX}{relative_path}{Fore.RESET}")
+        else:
+            print(f"{tab}Unknown: {node}")
 
 
 def main():
