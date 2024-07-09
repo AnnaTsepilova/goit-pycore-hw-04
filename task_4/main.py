d6f85a5e44d8e6bf824bb861cc7381423747d62c
Anna Tsepilova <virgo.tsav@gmail.com>
2024-07-09 22:13:48 +0300

    Added handler with commands

--- task_4/main.py
+++ task_4/main.py
@@ -1,34 +1,17 @@
+import handler
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
-
-def add_contact(args, contacts):
-    name, phone = args
-
-    if contacts.get(name):
-        return "Contact already exist."
-
-    contacts[name] = phone
-    return "Contact added."
-
-def list_contacts(contacts):
-    output = ""
-    for name, phone in contacts.items():
-        output = f"{output}Contact:{name} - {phone}\n"
-    return output
-
-def show_phone(args, contacts):
-    name = args[0]
-    phone = contacts.get(name)
-
-    return phone if phone else "Contact not found"
 
 def main():
     contacts = {}
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
+        if not user_input:
+            continue
         command, *args = parse_input(user_input)
 
         match command:
@@ -38,13 +21,13 @@ def main():
             case "hello":
                 print("How can I help you?")
             case "add":
-                print(add_contact(args, contacts))
+                print(handler.add_contact(args, contacts))
             case "change":
-                print(add_contact(args, contacts))
+                print(handler.change_contact(args, contacts))
             case "phone":
-                print(show_phone(args, contacts))
+                print(handler.show_phone(args, contacts))
             case "all":
-                print(list_contacts(contacts))
+                print(handler.list_contacts(contacts))
             case _:
                 print("Invalid command.")
 
