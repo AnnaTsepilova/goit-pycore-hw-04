a0f3867115c22015e3bd4fcb9e18d51582a8ee39
Anna Tsepilova <virgo.tsav@gmail.com>
2024-07-09 20:20:25 +0300

    Added comments, usage string, minor fixes

--- task_3/main.py
+++ task_3/main.py
@@ -1,25 +1,34 @@
-import sys, os
+import sys
+import os
 from pathlib import Path
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
-def parseFolder(path: str, deep: int = 1):
+def parse_folder(path: str, deep: int = 1):
+    """
+    Function scans directory and print internal items
+    Function is recursive!
+    """
     directory = Path(path)
+
+    ## Check provided path
     if not directory.is_dir():
-        print(f"{Fore.RED}{Style.BRIGHT}Error: incorrect path{Style.RESET_ALL}")
+        print(f"{Fore.RED}{Style.BRIGHT}Error: folder path does not exist or not found{Style.RESET_ALL}")
         return False
 
+    ## Tree indentation with spaces
     tab = '  ' * deep
 
     if deep == 1:
         print(f"{Fore.BLUE}{Style.BRIGHT}{path}/{Style.RESET_ALL}")
 
+    ## Loop for folder items
     for node in directory.iterdir():
         p_node = Path(node)
         relative_path = p_node.relative_to(path)
 
         if p_node.is_dir():
             print(f"{tab}{Fore.BLUE}{Style.BRIGHT}{relative_path}/{Style.RESET_ALL}")
-            parseFolder(node, deep + 1)
+            parse_folder(node, deep + 1)
         elif p_node.is_symlink():
             target = os.readlink(node)
             print(f"{tab}{Fore.CYAN}{node} -> {target}{Fore.RESET}")
@@ -31,7 +40,9 @@ def parseFolder(path: str, deep: int = 1):
 
 def main():
     if len(sys.argv) > 1:
-        parseFolder(sys.argv[1])
+        parse_folder(sys.argv[1])
+    else:
+        print(f"  {Fore.BLUE}Usage: main.py FOLDER_TO_SCAN{Style.RESET_ALL}")
 
 if __name__ == "__main__":
     main()
