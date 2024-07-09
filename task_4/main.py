2a8d8020918480b7a1ae11247865b300e282beaf
Anna Tsepilova <virgo.tsav@gmail.com>
2024-07-09 20:46:36 +0300

    Added task 4 dummy code

---
+++ task_4/main.py
@@ -0,0 +1,30 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        match command:
+            case "close" | "exit":
+                print("Good bye!")
+                break
+            case "hello":
+                print("How can I help you?")
+            case "add":
+                print(add_contact(args, contacts))
+            case _:
+                print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
